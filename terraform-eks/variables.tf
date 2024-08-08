d79376fb565afe017b02c6278de12b97f722b479
mbinui <99750582+mbinui@users.noreply.github.com>
2024-08-08 22:03:58 +0100

    Update variables.tf

--- terraform-eks/variables.tf
+++ terraform-eks/variables.tf
@@ -4,9 +4,30 @@ variable "aws_region" {
   default     = "us-east-2"
 }
 
-variable "cluster_name" {
-  description = "The name of the EKS cluster"
-  type        = string
-  default     = "my-voting-app-cluster"
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b"]
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  default     = "my-voting-app-cluster"
+}
